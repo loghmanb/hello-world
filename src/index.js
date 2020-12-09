113ef5ce749e8df0783ee6cfb0952ff4c7c433cd
Loghman Barari <barari@psg.net>
2020-12-09 17:38:40 +0330

    add MaterialUI

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  CssBaseline,
+  responsiveFontSizes,
+  } from "@material-ui/core";
+import { amber } from '@material-ui/core/colors';
 
 import App from "./components/App";
 
+let theme = createMuiTheme({
+  palette: {
+    primary: amber
+  },
+  root: {
+    padding: 100
+  }
+
+});
+
+theme = responsiveFontSizes(theme);  
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <CssBaseline />
+        <MuiThemeProvider theme={theme}>
+          <App />
+        </MuiThemeProvider>
     </Provider>
     , document.getElementById("app"));
